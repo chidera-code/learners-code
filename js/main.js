3562a202d6a758b558f3b07f6978069963f7f0ca
prosperemebo <emeboprosper@gmail.com>
2024-05-11 15:11:19 +0100

    Refactored code

--- js/main.js
+++ js/main.js
@@ -1,24 +1,39 @@
 const taskInput = document.getElementById('task');
 const addBtn = document.getElementById('add');
 const list = document.getElementById('list');
+const form = document.getElementById('form');
 
-addBtn.addEventListener('click', addTask);
-
-function addTask() {
+function addTask(event) {
   event.preventDefault();
-  const task = taskInput.value;
-  if (task !== '') {
-    const li = document.createElement('li');
-    li.textContent = task;
-    list.appendChild(li);
-    taskInput.value = '';
-  }
-}
+  const task = taskInput.value.trim();
 
-list.addEventListener('click', deleteTask);
+  const tasks = task.split(',');
+
+  tasks.forEach((task) => {
+    if (task.trim() !== '') {
+      const li = document.createElement('li');
+
+      li.textContent = task;
+      list.appendChild(li);
+
+      taskInput.value = '';
+    } else {
+      alert('Your input is not valid!');
+    }
+  });
+}
 
 function deleteTask(e) {
   if (e.target.tagName === 'LI') {
     e.target.remove();
   }
 }
+
+function main() {
+  list.addEventListener('click', deleteTask);
+  form.addEventListener('submit', addTask);
+
+  console.log('Application has started!');
+}
+
+main();
